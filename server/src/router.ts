a8aec656b4a4c0a55e7ea1b6f363b1cd5eed4450
Jose Montes <josemontesp@gmail.com>
2020-09-14 18:01:57 -0700

    Add error route to test sourcemaps

--- server/src/router.ts
+++ server/src/router.ts
@@ -7,3 +7,9 @@ export const router = express.Router();
 bookController.useRoute(router.route('/book'));
 
 router.use('/hello', (req, res) => res.send('Hi there!'));
+
+router.use('/error', (req, res) => {
+  const error = new Error('test erorr');
+
+  res.send(error.stack);
+});
